11c5f8e01db1bc8bd3d6c7e4c351a90c94b55127
Brian Gorman <briangorman99@gmail.com>
2023-06-30 12:40:40 -0400

    add function descriptions, add duration parameter for timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,25 @@ let timerEl = document.querySelector("#timer");
 
 let timeInterval;
 
+/**
+ * Clear timeInterval and set the text content to an empty string
+ */
 function clearTimer() {
   clearInterval(timeInterval);
   timerEl.textContent = "";
 }
 
-function startTimer() {
-  let time = 60;
-
+/**
+ * Start the timer and update the text content every second
+ * @param {number} duration How many seconds to run timer
+ */
+function startTimer(duration = 60) {
+  let time = duration;
   timeInterval = setInterval(function () {
     timerEl.textContent = "Time: " + time;
-    if (time === 0) clearTimer();
+    if (time === 0) {
+      clearTimer();
+    }
     time--;
   }, 1000);
 }
